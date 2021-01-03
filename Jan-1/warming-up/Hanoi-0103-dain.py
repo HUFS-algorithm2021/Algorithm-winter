29ed210c5f22676dc21fb4f79da4489552700360
Kang Dain <dmelli0505@gmail.com>
2021-01-03 23:32:26 +0900

    Kang Dain

--- Jan-1/warming-up/Hanoi-0103-dain.py
+++ Jan-1/warming-up/Hanoi-0103-dain.py
@@ -4,16 +4,14 @@
 # 따라서 n개의 원판을 옮기는 문제는 재귀적으로 n-1에 해당하는 하노이탑 함수를 호출함으로써 해결할 수 있다.
 def Hanoi(n, start, via, to):
     if n == 1:
-        print(start, to) # n = 1이면 경유지가 목적지
+        print(start, to) # n = 1이면 재귀 끝
     else:
-        Hanoi(n-1, start, via, to) # start에서 to를 거쳐서 via로 이동 
+        Hanoi(n-1, start, to, via) # start에서 to를 거쳐서 via로 이동 
         print(start, via)
-        Hanoi(n-1, via, to, start) # via에서 start를 거쳐서 to로 이동
+        Hanoi(n-1, via, start, to) # via에서 start를 거쳐서 to로 이동
 
 
 pan = int(input()) # 원 판의 갯수
-
-move_list = []
 
 print(pow(2, pan) - 1)
 Hanoi(pan, 1, 2, 3)
