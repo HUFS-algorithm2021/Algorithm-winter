cfb502451e3d4fcab141513c6363b73f5564abdd
Nakyung Im <nakyoung99@gmail.com>
2021-01-06 00:13:05 +0900

    Im Nakyung

--- Jan-2/warming-up/ReverseAdd-0105.py
+++ Jan-2/warming-up/ReverseAdd-0105.py
@@ -1,6 +1,6 @@
 # 백준 1357 임나경
-import sys
-x, y = map(int, sys.stdin.readline().split())
+
+x, y = map(int, input().split())
 x = int(str(x)[::-1])
 y = int(str(y)[::-1])
-print(str(x + y)[::-1])
+print(int(str(x + y)[::-1]))
