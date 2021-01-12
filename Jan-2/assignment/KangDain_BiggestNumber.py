31c47849bc1e96db67a7d819bec11adf8fb28938
Kang Dain <dmelli0505@gmail.com>
2021-01-12 23:35:14 +0900

    Kang Dain

--- Jan-2/assignment/KangDain_BiggestNumber.py
+++ Jan-2/assignment/KangDain_BiggestNumber.py
@@ -1,35 +1,17 @@
-N = int(input())
-num_list = list(map(int, input().split()))
-num = []
-for i in range(N):
-    temp = []
-    numTo = str(num_list[i])
-    for j in numTo:
-        temp.append(int(j))
-    temp.append(int(num_list[i]))
-    num.append(temp)
+def solution(numbers):
+    sum_num = sum(numbers)
+    # 문자열은 앞 문자가 큰 순서대로 & 길이가 긴 순서대로 sorting이 가능하다.
+    numbers = sorted(list(map(str, numbers)), key = lambda x:x*4, reverse = True)
+    # key 값에 각 요소의 문자열을 4번 반복해준 값을 기준으로 정렬하도록 했다. numbers의 최고값이 1000이기 때문에,가장 작은 수를 입력해도 4자릿수 이상이 되도록 했다.
+    # reverse를 통해서 앞 자리가 큰 수부터 뒤로 점진적으로 확인해서 정렬하도록 했다.
+    answer = ''
+    
+    if sum_num == 0:
+        answer = '0'
+    else:
+        answer = ''.join(numbers)
+    
+    return answer
 
-# num.sort(reverse=True)
-# # num.sort()
-
-for n in range(len(num) - 1):
-    for i in range(max(len(num[n]), len(num[n+1])) - 1):
-        if num[n][i] >= 10:
-            if num[n+1][i] > 0 and num[n+1][i] < 10:
-                num[n], num[n + 1] = num[n + 1], num[n]
-            else:
-                break
-        elif num[n+1][i] >= 10:
-            if num[n][i] > 0 and num[n][i] < 10:
-                num[n], num[n + 1] = num[n + 1], num[n]
-            else:
-                break
-        else:
-            if num[n][i] > num[n+1][i]:
-                num[n], num[n+1] = num[n+1], num[n]
-
-# print(num)
-s = ""
-for j in num:
-    s += str(j[-1])
-print(int(s))
+number = list(map(int, input().split()))
+print(solution(number))
