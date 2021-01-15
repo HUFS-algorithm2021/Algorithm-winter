1c6012dd0a3e6fd6b63bb56e5c2ae778817a22a3
Nakyung Im <nakyoung99@gmail.com>
2021-01-15 23:46:50 +0900

    Im Nakyung

---
+++ Jan-3/warming-up/OXQuix_0115_Nakyung.py.py
@@ -0,0 +1,17 @@
+num = int(input())
+list = []
+for i in range(num):
+    tmp2 = input()
+    list.append(tmp2)
+
+count = [0] * num
+tmp = 0
+for i in range(num):
+    for j in range(len(list[i])):
+        if list[i][j] == 'O':
+            tmp += 1
+            count[i] += tmp
+        else:
+            tmp = 0
+    tmp = 0
+    print(count[i])
