4c92a93172b64e0e3afcd5b6e3001b8ba52cfcfa
Nakyung Im <nakyoung99@gmail.com>
2021-01-19 01:37:56 +0900

    add comments

--- Jan-3/assignment/ImNakyung_DocumentSearch.py
+++ Jan-3/assignment/ImNakyung_DocumentSearch.py
@@ -1,13 +1,13 @@
 # 백준 1543 임나경
 
-doc = input()
-word = input()
+doc = input() # 문서
+word = input() # 검색할 단어
 count = 0
 tmp = ''
 for i in doc:
-    tmp += i
-    if tmp.find(word) != -1:
-        count += 1
-        tmp = ''
+    tmp += i # tmp에 i를 하나씩 더함
+    if tmp.find(word) != -1: # tmp에 word가 있으면
+        count += 1 # count 1 증가
+        tmp = '' # tmp 초기화
     else: pass
-print(count)
+print(count) # 몇 번 등장하는지 출력
