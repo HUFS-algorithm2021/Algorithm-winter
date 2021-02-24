79c76d059f5a6923d3bf1c0ccf55513446f4d36b
Kang Dain <dmelli0505@gmail.com>
2021-02-24 21:19:17 +0900

    KangDain

--- Feb-3/assignment/KangDain_NnM(2).py
+++ Feb-3/assignment/KangDain_NnM(2).py
@@ -1,6 +1,8 @@
-from itertools import combinations
+# 백준 15650 강다인
+
+from itertools import combinations # 조합 라이브러리
 
 N, M = map(int, input().split())
 
-for i in combinations(range(1, N+1), M):
+for i in combinations(range(1, N+1), M): # 1부터 n까지 중 m개를 뽑는 조합, 오름차순
     print(' '.join(list(map(str, i))))
